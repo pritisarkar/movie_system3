03dce21f1a693475a4508e2b33b995f30157f012
priti <priti.sarkar@ivanwebsolutions.com>
2024-10-28 12:51:27 +0530

    db setup

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movie_system3',
+        'USER' : 'postgres',
+        'PASSWORD' : 'root',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
     }
 }
 
